7a018b2cf57674f634894e90035247b14ecdfb97
teerryn <slaughterer90@gmail.com>
2019-06-09 18:02:38 +0100

    Change the variable name to creator

--- src/permissions.ts
+++ src/permissions.ts
@@ -9,11 +9,11 @@ const isAuthenticated = rule()(async (parent, args, ctx) => {
   return ctx.user !== null;
 });
 
-const isOwner = rule()(async (parent, args, ctx) => {
+const isCreator = rule()(async (parent, args, ctx) => {
   const userID = getUserId(ctx);
-  const owner = await ctx.prisma.job({ id: args.id }).creator();
-  ctx.user = owner
-  return userID === owner.id;
+  const creator = await ctx.prisma.job({ id: args.id }).creator();
+  ctx.user = creator
+  return userID === creator.id;
 });
 
 const isAdmin = rule()(async (parent, args, ctx: Context) => {
@@ -26,7 +26,7 @@ const isAdmin = rule()(async (parent, args, ctx: Context) => {
 const permissions = shield({
   Mutation: {
     publish: isAdmin, // only admin can publish jobs
-    unpublish: or(isAdmin, isOwner), // only admin or owner can unpublish a job
+    unpublish: or(isAdmin, isCreator), // only admin or creator can unpublish a job
     createJob: isAuthenticated // you need to me authenticated to create a job
   },
 });
